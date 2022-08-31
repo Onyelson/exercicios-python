70b41f2b8a363c4acdf74a0b55ee75b68023f1a2
Onyelson <100025066+Onyelson@users.noreply.github.com>
2022-08-31 17:27:42 -0300

    Update ex009.py

--- ex009.py
+++ ex009.py
@@ -1,6 +1,6 @@
 #tabuada de multiplicação
 
-tabuada = int(input('Tabuada do número: '))
+num = int(input('Tabuada de multiplicação do número: '))
 print('{} x {} = {}'.format(num, 1, num*1))
 print('{} x {} = {}'.format(num, 2, num*2))
 print('{} x {} = {}'.format(num, 3, num*3))
