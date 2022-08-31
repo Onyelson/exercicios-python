c41ad8fadf1e1d1fa4ee8f1aec01843847a46a58
Onyelson <100025066+Onyelson@users.noreply.github.com>
2022-08-31 17:53:54 -0300

    Update ex011.py

--- ex011.py
+++ ex011.py
@@ -3,4 +3,4 @@ l = float(input('Qual a largura da parede(m): '))
 a = float(input('Qual a altura da parede(m): '))
 ar = l * a
 lt = ar / 2
-print('Em uma área de {}m2 serão necessários {}l de tinta.'.format(ar, lt))
+print('Em uma área de {}m² serão necessários {}l de tinta.'.format(ar, lt))
